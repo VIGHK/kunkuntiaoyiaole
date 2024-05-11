a7b5b412ac813bfa556582440e8f34eb124442fe
VIGHK <160388541+VIGHK@users.noreply.github.com>
2024-05-11 23:18:02 +0800

    Add files via upload

---
+++ map.cpp
@@ -0,0 +1,34 @@
+#include<map.h>
+#include<config.h>
+Map::Map()
+{
+    //加载地图
+    m_map1.load(MAP_PATH);
+    m_map2.load(MAP_PATH);
+
+
+    //初始化X轴
+    m_map1_posX = GAME_WIDTH;
+    m_map2_posX = 0;
+
+    //滚动速度
+    m_scroll_speed = MAP_SCROLL_SPEED;
+}
+
+void Map::mapPosition()
+{
+    //第一张滚动
+    m_map1_posX -= m_scroll_speed;     //向左
+    if(m_map1_posX <= 0)
+    {
+        m_map1_posX = GAME_WIDTH;
+    }
+
+    //第二张
+    m_map2_posX -= m_scroll_speed;
+    if(m_map2_posX <= -GAME_WIDTH)
+    {
+        m_map2_posX = 0;
+    }
+
+}
---
+++ map.h
@@ -0,0 +1,28 @@
+#ifndef MAP_H
+#define MAP_H
+#include <QPixmap>
+
+//地图类
+class Map{
+
+public:
+
+    Map();
+    //地图滚动坐标计算
+    void mapPosition();
+public:
+
+    QPixmap m_map1;
+    QPixmap m_map2;
+
+    //地图X轴坐标
+    int m_map1_posX;
+    int m_map2_posX;
+
+
+
+    int m_scroll_speed;
+
+};
+
+#endif // MAP_H
